02ab206450f5299ceb07bcb892f4168eaea73ca4
allensam88 <54912919+allensam88@users.noreply.github.com>
2020-03-18 20:02:46 -0700

    got cookie monster done

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python
 # STEP 1: UNDERSTAND
-# All possible permutations for 5 cookies
-# 1.	1+1+1+1+1
-# 2.	1+1+1+2
-# 3.	1+1+2+1
-# 4. 	1+2+1+1
-# 5. 	2+1+1+1
-# 6. 	1+2+2
-# 7. 	2+1+2
-# 8. 	2+2+1
-# 9. 	1+1+3
-# 10. 	1+3+1
-# 11. 	3+1+1
-# 12. 	2+3
-# 13. 	3+2
+# Fibonacci Sequence for 5
+# 0, 1, 1, 2, 3, 5
+# equals 13 if you count 0 as 1
 
 import sys
 
@@ -23,10 +12,11 @@ import sys
 
 
 def eating_cookies(n, cache=None):
-    if n <= 0:
-		return 1
-	if n > 0:
-		return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
+    if n <= 1:
+        return 1
+    if n <= 2:
+        return 2
+    return eating_cookies(n-3) + eating_cookies(n - 2) + eating_cookies(n - 1)
 
 
 if __name__ == "__main__":
